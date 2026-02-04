db48d84b772e9c2b07e60e1880f94388a7f91f51
user <ucheokeke16@gmail.com>
2026-02-04 11:26:17 +0000

    create roles and permissions check

--- TeamOps/src/middleware/auth.middleware.ts
+++ TeamOps/src/middleware/auth.middleware.ts
@@ -133,9 +133,53 @@ export const authMiddleware = (req: AuthRequest, res: Response, next: NextFuncti
             return next();
         }
 
+        const payload = JWTService.verifyAccessToken(token);
+        if (!payload) { // invalid/expired token
+            req.user = undefined;
+            return next();
+        }
+
+        req.user = payload;
+        req.ip = req.ip || req.socket.remoteAddress || 'unknown';
+        req.userAgent = req.get('user-agent') || 'unknown';
+        next();
         
     } catch (error) {
-        console.error("");
+        console.error('Auth middleware error: ', error);
         next();
     }
 }
+
+// Authorization Guards 
+
+export const requireAuth = (req: AuthRequest, res: Response, next: NextFunction) => {
+    if (!req.user) {
+        return res.status(401).json({
+            error: 'UNAUTHORIZED',
+            message: 'Authentication required'
+        });
+    }
+    next();
+};
+
+export const requireRole = (...roles: UserRole[]) => {
+    return (req: AuthRequest, res: Response, next: NextFunction) => {
+        if (!req.user) {
+            return res.status(401).json({
+                error: 'UNAUTHORIZED',
+                message: 'Authentication required'
+            });
+        };
+
+        if (!roles.includes(req.user.role)) {
+            return res.status(301).json({
+                error: 'INSUFFICIENT_PERMISSION',
+                message: `Requires one of ${roles.join(',' )}`
+            });
+        };
+
+        next();
+    }
+}
+
+export const requirePermission = () => {}
